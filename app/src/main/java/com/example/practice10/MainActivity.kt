cecd102c990ec5ed9797da406ad816f07b2d3209
Your Name <you@example.com>
2023-10-01 17:21:34 +0300

    first commit

--- app/src/main/java/com/example/practice10/MainActivity.kt
+++ app/src/main/java/com/example/practice10/MainActivity.kt
@@ -1,26 +1,42 @@
 package com.example.practice10
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var button: Button
+    lateinit var button2:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setContentView(R.layout.linerlayout)
+        setTitle("LinerLayout")
+        setContentView(R.layout.tablelayout)
+        setTitle("TableLayout")
+       button=findViewById(R.id.button)
+        button2=findViewById(R.id.button2)
+        button.setOnClickListener {
+            button.visibility=View.INVISIBLE
+            button2.visibility=View.VISIBLE
+        }
+        button2.setOnClickListener{
+            button.visibility=View.INVISIBLE
+            button2.visibility=View.VISIBLE
+        }
     }
 
+
     fun ButtonClick(view: View) {
-        val b1 : Button = findViewById(R.id.button1)
-        val b2 : Button = findViewById(R.id.button2)
-        if (b1.isClickable){
-            b1.visibility = View.INVISIBLE
-            b2.visibility = View.VISIBLE
-        }
-        else {
-            b1.visibility = View.VISIBLE
-            b2.visibility = View.INVISIBLE
-        }
+
+
+
+
     }
+
+
 }
